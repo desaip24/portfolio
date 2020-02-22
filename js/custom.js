f6d51242fb34e1abc37fc0f0ad2fbf0dbad93423
desaip24 <p142desai@gmail.com>
2020-02-22 12:00:46 +0530

    testimonials added

--- js/custom.js
+++ js/custom.js
@@ -12,13 +12,28 @@
     $(".navbar").headroom();
 
     // PROJECT CAROUSEL
-    $('.owl-carousel').owlCarousel({
-    	items: 1,
-	    loop:true,
-	    margin:10,
-	    nav:true
-	});
 
+    $('#testimonials-list').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots:true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1170: {
+                items: 3
+            }
+        }
+    });
     // SMOOTHSCROLL
     $(function() {
       $('.nav-link, .custom-btn-link').on('click', function(event) {
