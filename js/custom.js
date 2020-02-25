ab8cb9ee4fed244206deb4d9d07c7e5c36bccde8
Parth Desai <1594417@TCSPFIZER.COM>
2020-02-25 12:25:05 +0530

    fullpage.js is stable

--- js/custom.js
+++ js/custom.js
@@ -34,15 +34,15 @@
         }
     });
     // SMOOTHSCROLL
-    $(function() {
-      $('.nav-link, .custom-btn-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });  
+    // $(function() {
+    //   $('.nav-link, .custom-btn-link').on('click', function(event) {
+    //     var $anchor = $(this);
+    //     $('html, body').stop().animate({
+    //         scrollTop: $($anchor.attr('href') + '-section').offset().top - 49
+    //     }, 1000);
+    //     event.preventDefault();
+    //   });
+    // });  
 
     // TOOLTIP
     $('.social-links a').tooltip();
