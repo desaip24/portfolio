4dc131628f1b62d744bbedb8d9ff66521eac5e20
desaip24 <p142desai@gmail.com>
2020-03-11 23:55:40 +0530

    jainam

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,51 @@
 (function ($) {
   "use strict";
+ 
+
+// scroll function
+function scrollToID(id, speed){
+	var offSet = 50;
+	var targetOffset = $(id).offset().top - offSet;
+	var mainNav = $('#main-nav');
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+	if (mainNav.hasClass("open")) {
+		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+		mainNav.removeClass("open");
+	}
+}
+
 
   //NAV BAR ACTIVE CLASS
   $(".nav a").on("click", function(){
      $(".nav").find(".active").removeClass("active");
      $(this).parent().addClass("active");
+   
   })
+  window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 0.9(screen.width) || document.documentElement.scrollTop > 0.9(screen.height)) {
+    document.getElementById("hoverHome").style.color = "red";
+    
+  }
+  else if (document.body.scrollTop > 0.6(screen.width) || document.documentElement.scrollTop > 0.6(screen.height)) {
+    document.getElementById("hoverAbout").style.color = "pink";
+    
+  }
+   else if (document.body.scrollTop > 0.4(screen.width)|| document.documentElement.scrollTop > 0.4(screen.height)) {
+    document.getElementById("hoverProjects").style.color = "violet";
+    
+  }
+  else if (document.body.scrollTop > 0.2(screen.width) || document.documentElement.scrollTop > 0.2(screen.height)) {
+    document.getElementById("hoverTestimonials").style.color = "green";
+    
+  } else {
+    document.getElementById("projects").style.padding = "pink";
+    
+  }
+}
+
+ 
   // HEADER
   // $(".navbar").headroom();
 
@@ -57,3 +96,4 @@
   });
 
 })(jQuery);
+
